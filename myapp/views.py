14e7d2ca939d0c2099f9c6dd840c05dea34ebe0d
aa <>
2021-09-21 18:57:51 +0900

    image表示できた

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from . import create_csv 
 
 # Create your views here.
 def homework(request):
@@ -7,9 +8,16 @@ def homework(request):
 def index(request):
     return render(request, 'index.html') 
 
+#映画ジャンルを受け取る
 def recommend(request):
+    result = request.POST.getlist("movie")
+    #print(result)
+    if request.method == "POST":
+        # recommend.py呼びだす
+        movies = create_csv.get_movie(result)
+            
+        return render(request, 'fact.html', { "movies": movies })
     return render(request, 'recommend.html') 
 
 def fact(request):
     return render(request, 'fact.html') 
-    
